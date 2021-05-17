db879e9810786bca374736abbd52a1ebb1701604
NelsonUprety <nelsonuprety3@gmail.com>
2021-05-17 12:28:09 +0545

    About promise, promise all, async await

--- promises.js
+++ promises.js
@@ -18,7 +18,7 @@ function createPost(post) {
     setTimeout(function () {
       posts.push(post);
 
-      const error = true;
+      const error = false;
 
       if (!error) {
         resolve();
@@ -29,6 +29,38 @@ function createPost(post) {
   });
 }
 
-// createPost({ title: 'Post Three', body: 'This is post three' })
-//   .then(getPosts)
-//   .catch(err => console.log(err));
+createPost({ title: 'Post Three', body: 'This is post three' })
+  .then(getPosts)
+  .catch(err => console.log(err));
+
+// Async / Await
+async function init() {
+  await createPost({ title: 'Post Three', body: 'This is post three' });
+
+  getPosts();
+}
+
+init();
+
+// Async await with fetch
+async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+  const data = await res.json();
+  console.log(data);
+}
+fetchUsers();
+
+// Promise.all
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 2000, 'GoodBye')
+);
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res =>
+  res.json()
+);
+
+Promise.all([promise1, promise2, promise3, promise4]).then(values =>
+  console.log(values)
+);
